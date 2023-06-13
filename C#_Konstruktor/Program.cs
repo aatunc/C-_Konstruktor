9627aac84f31627eb98945e931c80ce1b3dcc0a0
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-06-13 13:27:57 +0200

    Projektdateien hinzufügen.

---
+++ C#_Konstruktor/Program.cs
@@ -0,0 +1,13 @@
+﻿class Dog
+{
+    public string Name { get; set; }
+    public int Jahr { get; set; }
+    public string Breed { get; set; }
+
+    public Dog(string name, int jahr, string Bread)
+    {
+        Name = name;
+        Jahr = jahr;
+        Bread = breed;
+    }
+}
